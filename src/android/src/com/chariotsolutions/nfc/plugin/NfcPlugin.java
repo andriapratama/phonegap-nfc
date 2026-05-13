124945e8c671c43ff340263aa62ecf1224220c01
Andria Pratama <ptandriapratama@gmail.com>
2026-05-13 15:49:03 +0800

    update

--- src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
+++ src/android/src/com/chariotsolutions/nfc/plugin/NfcPlugin.java
@@ -479,20 +479,19 @@ public class NfcPlugin extends CordovaPlugin implements NfcAdapter.OnNdefPushCom
     }
 
     private void createPendingIntent() {
-    if (pendingIntent == null) {
-        Activity activity = getActivity();
-        Intent intent = new Intent(activity, activity.getClass());
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        
-        int flags = 0; 
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
-            flags = PendingIntent.FLAG_IMMUTABLE;
+        if (pendingIntent == null) {
+            Activity activity = getActivity();
+            Intent intent = new Intent(activity, activity.getClass());
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            
+            int flags = PendingIntent.FLAG_SINGLE_TOP;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+                flags |= PendingIntent.FLAG_MUTABLE; 
+            }
+            
+            pendingIntent = PendingIntent.getActivity(activity, 0, intent, flags);
         }
-
-        pendingIntent = PendingIntent.getActivity(activity, 0, intent, flags);
     }
-}
 
     private void addTechList(String[] list) {
         this.addTechFilter();
